5159edc60e2bf49e3e7c1b658e47051bd4f37aae
EdwardXie <edwardxie1@gmail.com>
2020-04-19 14:35:37 -0400

    sprite dictionary

---
+++ client/src/library/sprites/sprites.js
@@ -0,0 +1,627 @@
+import down001 from '../assets/sprites/001_down.gif'
+import left001 from '../assets/sprites/001_left.gif'
+import right001 from '../assets/sprites/001_right.gif'
+import up001 from '../assets/sprites/001_up.gif'
+import down002 from '../assets/sprites/002_down.gif'
+import left002 from '../assets/sprites/002_left.gif'
+import right002 from '../assets/sprites/002_right.gif'
+import up002 from '../assets/sprites/002_up.gif'
+import down003 from '../assets/sprites/003_down.gif'
+import left003 from '../assets/sprites/003_left.gif'
+import right003 from '../assets/sprites/003_right.gif'
+import up003 from '../assets/sprites/003_up.gif'
+import down004 from '../assets/sprites/004_down.gif'
+import left004 from '../assets/sprites/004_left.gif'
+import right004 from '../assets/sprites/004_right.gif'
+import up004 from '../assets/sprites/004_up.gif'
+import down005 from '../assets/sprites/005_down.gif'
+import left005 from '../assets/sprites/005_left.gif'
+import right005 from '../assets/sprites/005_right.gif'
+import up005 from '../assets/sprites/005_up.gif'
+import down006 from '../assets/sprites/006_down.gif'
+import left006 from '../assets/sprites/006_left.gif'
+import right006 from '../assets/sprites/006_right.gif'
+import up006 from '../assets/sprites/006_up.gif'
+import down007 from '../assets/sprites/007_down.gif'
+import left007 from '../assets/sprites/007_left.gif'
+import right007 from '../assets/sprites/007_right.gif'
+import up007 from '../assets/sprites/007_up.gif'
+import down008 from '../assets/sprites/008_down.gif'
+import left008 from '../assets/sprites/008_left.gif'
+import right008 from '../assets/sprites/008_right.gif'
+import up008 from '../assets/sprites/008_up.gif'
+import down009 from '../assets/sprites/009_down.gif'
+import left009 from '../assets/sprites/009_left.gif'
+import right009 from '../assets/sprites/009_right.gif'
+import up009 from '../assets/sprites/009_up.gif'
+import down018 from '../assets/sprites/018_down.gif'
+import left018 from '../assets/sprites/018_left.gif'
+import right018 from '../assets/sprites/018_right.gif'
+import up018 from '../assets/sprites/018_up.gif'
+import down025 from '../assets/sprites/025_down.gif'
+import left025 from '../assets/sprites/025_left.gif'
+import right025 from '../assets/sprites/025_right.gif'
+import up025 from '../assets/sprites/025_up.gif'
+import down026 from '../assets/sprites/026_down.gif'
+import left026 from '../assets/sprites/026_left.gif'
+import right026 from '../assets/sprites/026_right.gif'
+import up026 from '../assets/sprites/026_up.gif'
+import down031 from '../assets/sprites/031_down.gif'
+import left031 from '../assets/sprites/031_left.gif'
+import right031 from '../assets/sprites/031_right.gif'
+import up031 from '../assets/sprites/031_up.gif'
+import down037 from '../assets/sprites/037_down.gif'
+import left037 from '../assets/sprites/037_left.gif'
+import right037 from '../assets/sprites/037_right.gif'
+import up037 from '../assets/sprites/037_up.gif'
+import down038 from '../assets/sprites/038_down.gif'
+import left038 from '../assets/sprites/038_left.gif'
+import right038 from '../assets/sprites/038_right.gif'
+import up038 from '../assets/sprites/038_up.gif'
+import down058 from '../assets/sprites/058_down.gif'
+import left058 from '../assets/sprites/058_left.gif'
+import right058 from '../assets/sprites/058_right.gif'
+import up058 from '../assets/sprites/058_up.gif'
+import down059 from '../assets/sprites/059_down.gif'
+import left059 from '../assets/sprites/059_left.gif'
+import right059 from '../assets/sprites/059_right.gif'
+import up059 from '../assets/sprites/059_up.gif'
+import down062 from '../assets/sprites/062_down.gif'
+import left062 from '../assets/sprites/062_left.gif'
+import right062 from '../assets/sprites/062_right.gif'
+import up062 from '../assets/sprites/062_up.gif'
+import down064 from '../assets/sprites/064_down.gif'
+import left064 from '../assets/sprites/064_left.gif'
+import right064 from '../assets/sprites/064_right.gif'
+import up064 from '../assets/sprites/064_up.gif'
+import down065 from '../assets/sprites/065_down.gif'
+import left065 from '../assets/sprites/065_left.gif'
+import right065 from '../assets/sprites/065_right.gif'
+import up065 from '../assets/sprites/065_up.gif'
+import down068 from '../assets/sprites/068_down.gif'
+import left068 from '../assets/sprites/068_left.gif'
+import right068 from '../assets/sprites/068_right.gif'
+import up068 from '../assets/sprites/068_up.gif'
+import down076 from '../assets/sprites/076_down.gif'
+import left076 from '../assets/sprites/076_left.gif'
+import right076 from '../assets/sprites/076_right.gif'
+import up076 from '../assets/sprites/076_up.gif'
+import down078 from '../assets/sprites/078_down.gif'
+import left078 from '../assets/sprites/078_left.gif'
+import right078 from '../assets/sprites/078_right.gif'
+import up078 from '../assets/sprites/078_up.gif'
+import down093 from '../assets/sprites/093_down.gif'
+import left093 from '../assets/sprites/093_left.gif'
+import right093 from '../assets/sprites/093_right.gif'
+import up093 from '../assets/sprites/093_up.gif'
+import down094 from '../assets/sprites/094_down.gif'
+import left094 from '../assets/sprites/094_left.gif'
+import right094 from '../assets/sprites/094_right.gif'
+import up094 from '../assets/sprites/094_up.gif'
+import down095 from '../assets/sprites/095_down.gif'
+import left095 from '../assets/sprites/095_left.gif'
+import right095 from '../assets/sprites/095_right.gif'
+import up095 from '../assets/sprites/095_up.gif'
+import down104 from '../assets/sprites/104_down.gif'
+import left104 from '../assets/sprites/104_left.gif'
+import right104 from '../assets/sprites/104_right.gif'
+import up104 from '../assets/sprites/104_up.gif'
+import down105 from '../assets/sprites/105_down.gif'
+import left105 from '../assets/sprites/105_left.gif'
+import right105 from '../assets/sprites/105_right.gif'
+import up105 from '../assets/sprites/105_up.gif'
+import down106 from '../assets/sprites/106_down.gif'
+import left106 from '../assets/sprites/106_left.gif'
+import right106 from '../assets/sprites/106_right.gif'
+import up106 from '../assets/sprites/106_up.gif'
+import down107 from '../assets/sprites/107_down.gif'
+import left107 from '../assets/sprites/107_left.gif'
+import right107 from '../assets/sprites/107_right.gif'
+import up107 from '../assets/sprites/107_up.gif'
+import down123 from '../assets/sprites/123_down.gif'
+import left123 from '../assets/sprites/123_left.gif'
+import right123 from '../assets/sprites/123_right.gif'
+import up123 from '../assets/sprites/123_up.gif'
+import down125 from '../assets/sprites/125_down.gif'
+import left125 from '../assets/sprites/125_left.gif'
+import right125 from '../assets/sprites/125_right.gif'
+import up125 from '../assets/sprites/125_up.gif'
+import down126 from '../assets/sprites/126_down.gif'
+import left126 from '../assets/sprites/126_left.gif'
+import right126 from '../assets/sprites/126_right.gif'
+import up126 from '../assets/sprites/126_up.gif'
+import down129 from '../assets/sprites/129_down.gif'
+import left129 from '../assets/sprites/129_left.gif'
+import right129 from '../assets/sprites/129_right.gif'
+import up129 from '../assets/sprites/129_up.gif'
+import down130 from '../assets/sprites/130_down.gif'
+import left130 from '../assets/sprites/130_left.gif'
+import right130 from '../assets/sprites/130_right.gif'
+import up130 from '../assets/sprites/130_up.gif'
+import down131 from '../assets/sprites/131_down.gif'
+import left131 from '../assets/sprites/131_left.gif'
+import right131 from '../assets/sprites/131_right.gif'
+import up131 from '../assets/sprites/131_up.gif'
+import down132 from '../assets/sprites/132_down.gif'
+import left132 from '../assets/sprites/132_left.gif'
+import right132 from '../assets/sprites/132_right.gif'
+import up132 from '../assets/sprites/132_up.gif'
+import down133 from '../assets/sprites/133_down.gif'
+import left133 from '../assets/sprites/133_left.gif'
+import right133 from '../assets/sprites/133_right.gif'
+import up133 from '../assets/sprites/133_up.gif'
+import down134 from '../assets/sprites/134_down.gif'
+import left134 from '../assets/sprites/134_left.gif'
+import right134 from '../assets/sprites/134_right.gif'
+import up134 from '../assets/sprites/134_up.gif'
+import down135 from '../assets/sprites/135_down.gif'
+import left135 from '../assets/sprites/135_left.gif'
+import right135 from '../assets/sprites/135_right.gif'
+import up135 from '../assets/sprites/135_up.gif'
+import down136 from '../assets/sprites/136_down.gif'
+import left136 from '../assets/sprites/136_left.gif'
+import right136 from '../assets/sprites/136_right.gif'
+import up136 from '../assets/sprites/136_up.gif'
+import down141 from '../assets/sprites/141_down.gif'
+import left141 from '../assets/sprites/141_left.gif'
+import right141 from '../assets/sprites/141_right.gif'
+import up141 from '../assets/sprites/141_up.gif'
+import down142 from '../assets/sprites/142_down.gif'
+import left142 from '../assets/sprites/142_left.gif'
+import right142 from '../assets/sprites/142_right.gif'
+import up142 from '../assets/sprites/142_up.gif'
+import down143 from '../assets/sprites/143_down.gif'
+import left143 from '../assets/sprites/143_left.gif'
+import right143 from '../assets/sprites/143_right.gif'
+import up143 from '../assets/sprites/143_up.gif'
+import down144 from '../assets/sprites/144_down.gif'
+import left144 from '../assets/sprites/144_left.gif'
+import right144 from '../assets/sprites/144_right.gif'
+import up144 from '../assets/sprites/144_up.gif'
+import down145 from '../assets/sprites/145_down.gif'
+import left145 from '../assets/sprites/145_left.gif'
+import right145 from '../assets/sprites/145_right.gif'
+import up145 from '../assets/sprites/145_up.gif'
+import down146 from '../assets/sprites/146_down.gif'
+import left146 from '../assets/sprites/146_left.gif'
+import right146 from '../assets/sprites/146_right.gif'
+import up146 from '../assets/sprites/146_up.gif'
+import down147 from '../assets/sprites/147_down.gif'
+import left147 from '../assets/sprites/147_left.gif'
+import right147 from '../assets/sprites/147_right.gif'
+import up147 from '../assets/sprites/147_up.gif'
+import down148 from '../assets/sprites/148_down.gif'
+import left148 from '../assets/sprites/148_left.gif'
+import right148 from '../assets/sprites/148_right.gif'
+import up148 from '../assets/sprites/148_up.gif'
+import down149 from '../assets/sprites/149_down.gif'
+import left149 from '../assets/sprites/149_left.gif'
+import right149 from '../assets/sprites/149_right.gif'
+import up149 from '../assets/sprites/149_up.gif'
+import down150 from '../assets/sprites/150_down.gif'
+import left150 from '../assets/sprites/150_left.gif'
+import right150 from '../assets/sprites/150_right.gif'
+import up150 from '../assets/sprites/150_up.gif'
+import down151 from '../assets/sprites/151_down.gif'
+import left151 from '../assets/sprites/151_left.gif'
+import right151 from '../assets/sprites/151_right.gif'
+import up151 from '../assets/sprites/151_up.gif'
+
+export default [
+  {
+    name: "Bulbasaur",
+    srcDown: down001,
+    srcUp: up001,
+    srcleft: left001,
+    srcRight: right001,
+    picked: false
+  },
+  {
+    name: "Ivysaur",
+    srcDown: down002,
+    srcUp: up002,
+    srcleft: left002,
+    srcRight: right002,
+    picked: false
+  },
+  {
+    name: "Venusaur",
+    srcDown: down003,
+    srcUp: up003,
+    srcleft: left003,
+    srcRight: right003,
+    picked: false
+  },
+  {
+    name: "Charmander",
+    srcDown: down004,
+    srcUp: up004,
+    srcleft: left004,
+    srcRight: right004,
+    picked: false
+  },
+  {
+    name: "Charmeleon",
+    srcDown: down005,
+    srcUp: up005,
+    srcleft: left005,
+    srcRight: right005,
+    picked: false
+  },
+  {
+    name: "	Charizard",
+    srcDown: down006,
+    srcUp: up006,
+    srcleft: left006,
+    srcRight: right006,
+    picked: false
+  },
+  {
+    name: "Squirtle",
+    srcDown: down007,
+    srcUp: up007,
+    srcleft: left007,
+    srcRight: right007,
+    picked: false
+  },
+  {
+    name: "Wartortle",
+    srcDown: down008,
+    srcUp: up008,
+    srcleft: left008,
+    srcRight: right008,
+    picked: false
+  },
+  {
+    name: "Blastoise",
+    srcDown: down009,
+    srcUp: up009,
+    srcleft: left009,
+    srcRight: right009,
+    picked: false
+  },
+  {
+    name: "Pidgeot",
+    srcDown: down018,
+    srcUp: up018,
+    srcleft: left018,
+    srcRight: right018,
+    picked: false
+  },
+  {
+    name: "Pikachu",
+    srcDown: down025,
+    srcUp: up025,
+    srcleft: left025,
+    srcRight: right025,
+    picked: false
+  },
+  {
+    name: "Raichu",
+    srcDown: down026,
+    srcUp: up026,
+    srcleft: left026,
+    srcRight: right026,
+    picked: false
+  },
+  {
+    name: "Nidoqueen",
+    srcDown: down031,
+    srcUp: up031,
+    srcleft: left031,
+    srcRight: right031,
+    picked: false
+  },
+  {
+    name: "Vulpix",
+    srcDown: down037,
+    srcUp: up037,
+    srcleft: left037,
+    srcRight: right037,
+    picked: false
+  },
+  {
+    name: "Ninetales",
+    srcDown: down038,
+    srcUp: up038,
+    srcleft: left038,
+    srcRight: right038,
+    picked: false
+  },
+  {
+    name: "Growlithe",
+    srcDown: down058,
+    srcUp: up058,
+    srcleft: left058,
+    srcRight: right058,
+    picked: false
+  },
+  {
+    name: "Arcanine",
+    srcDown: down059,
+    srcUp: up059,
+    srcleft: left059,
+    srcRight: right059,
+    picked: false
+  },
+  {
+    name: "Poliwrath",
+    srcDown: down062,
+    srcUp: up062,
+    srcleft: left062,
+    srcRight: right062,
+    picked: false
+  },
+  {
+    name: "Kadabra",
+    srcDown: down064,
+    srcUp: up064,
+    srcleft: left064,
+    srcRight: right064,
+    picked: false
+  },
+  {
+    name: "Alakazam",
+    srcDown: down065,
+    srcUp: up065,
+    srcleft: left065,
+    srcRight: right065,
+    picked: false
+  },
+  {
+    name: "Machamp",
+    srcDown: down068,
+    srcUp: up068,
+    srcleft: left068,
+    srcRight: right068,
+    picked: false
+  },
+  {
+    name: "Golem",
+    srcDown: down076,
+    srcUp: up076,
+    srcleft: left076,
+    srcRight: right076,
+    picked: false
+  },
+  {
+    name: "Rapidash",
+    srcDown: down078,
+    srcUp: up078,
+    srcleft: left078,
+    srcRight: right078,
+    picked: false
+  },
+  {
+    name: "Haunter",
+    srcDown: down093,
+    srcUp: up093,
+    srcleft: left093,
+    srcRight: right093,
+    picked: false
+  },
+  {
+    name: "Gengar",
+    srcDown: down094,
+    srcUp: up094,
+    srcleft: left094,
+    srcRight: right094,
+    picked: false
+  },
+  {
+    name: "Onix",
+    srcDown: down095,
+    srcUp: up095,
+    srcleft: left095,
+    srcRight: right095,
+    picked: false
+  },
+  {
+    name: "Cubone",
+    srcDown: down104,
+    srcUp: up104,
+    srcleft: left104,
+    srcRight: right104,
+    picked: false
+  },
+  {
+    name: "Marowak",
+    srcDown: down105,
+    srcUp: up105,
+    srcleft: left105,
+    srcRight: right105,
+    picked: false
+  },
+  {
+    name: "Hitmonlee",
+    srcDown: down106,
+    srcUp: up106,
+    srcleft: left106,
+    srcRight: right106,
+    picked: false
+  },
+  {
+    name: "Hitmonchan",
+    srcDown: down107,
+    srcUp: up107,
+    srcleft: left107,
+    srcRight: right107,
+    picked: false
+  },
+  {
+    name: "Scyther",
+    srcDown: down123,
+    srcUp: up123,
+    srcleft: left123,
+    srcRight: right123,
+    picked: false
+  },
+  {
+    name: "Electabuzz",
+    srcDown: down125,
+    srcUp: up125,
+    srcleft: left125,
+    srcRight: right125,
+    picked: false
+  },
+  {
+    name: "Magmar",
+    srcDown: down126,
+    srcUp: up126,
+    srcleft: left126,
+    srcRight: right126,
+    picked: false
+  },
+  {
+    name: "Magikarp",
+    srcDown: down129,
+    srcUp: up129,
+    srcleft: left129,
+    srcRight: right129,
+    picked: false
+  },
+  {
+    name: "Gyarados",
+    srcDown: down130,
+    srcUp: up130,
+    srcleft: left130,
+    srcRight: right130,
+    picked: false
+  },
+  {
+    name: "Lapras",
+    srcDown: down131,
+    srcUp: up131,
+    srcleft: left131,
+    srcRight: right131,
+    picked: false
+  },
+  {
+    name: "Ditto",
+    srcDown: down132,
+    srcUp: up132,
+    srcleft: left132,
+    srcRight: right132,
+    picked: false
+  },
+  {
+    name: "Eevee",
+    srcDown: down133,
+    srcUp: up133,
+    srcleft: left133,
+    srcRight: right133,
+    picked: false
+  },
+  {
+    name: "Vaporeon",
+    srcDown: down134,
+    srcUp: up134,
+    srcleft: left134,
+    srcRight: right134,
+    picked: false
+  },
+  {
+    name: "Jolteon",
+    srcDown: down135,
+    srcUp: up135,
+    srcleft: left135,
+    srcRight: right135,
+    picked: false
+  },
+  {
+    name: "Flareon",
+    srcDown: down136,
+    srcUp: up136,
+    srcleft: left136,
+    srcRight: right136,
+    picked: false
+  },
+  {
+    name: "Kabutops",
+    srcDown: down141,
+    srcUp: up141,
+    srcleft: left141,
+    srcRight: right141,
+    picked: false
+  },
+  {
+    name: "Areodactyl",
+    srcDown: down142,
+    srcUp: up142,
+    srcleft: left142,
+    srcRight: right142,
+    picked: false
+  },
+  {
+    name: "Snorlax",
+    srcDown: down143,
+    srcUp: up143,
+    srcleft: left143,
+    srcRight: right143,
+    picked: false
+  },
+  {
+    name: "Articuno",
+    srcDown: down144,
+    srcUp: up144,
+    srcleft: left144,
+    srcRight: right144,
+    picked: false
+  },
+  {
+    name: "Zapdos",
+    srcDown: down145,
+    srcUp: up145,
+    srcleft: left145,
+    srcRight: right145,
+    picked: false
+  },
+  {
+    name: "Moltres",
+    srcDown: down146,
+    srcUp: up146,
+    srcleft: left146,
+    srcRight: right146,
+    picked: false
+  },
+  {
+    name: "Dratini",
+    srcDown: down147,
+    srcUp: up147,
+    srcleft: left147,
+    srcRight: right147,
+    picked: false
+  },
+  {
+    name: "Dragonair",
+    srcDown: down148,
+    srcUp: up148,
+    srcleft: left148,
+    srcRight: right148,
+    picked: false
+  },
+  {
+    name: "Dragonite",
+    srcDown: down149,
+    srcUp: up149,
+    srcleft: left149,
+    srcRight: right149,
+    picked: false
+  },
+  {
+    name: "Mewtwo",
+    srcDown: down150,
+    srcUp: up150,
+    srcleft: left150,
+    srcRight: right150,
+    picked: false
+  },
+  {
+    name: "Mew",
+    srcDown: down151,
+    srcUp: up151,
+    srcleft: left151,
+    srcRight: right151,
+    picked: false
+  }
+]
